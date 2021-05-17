96d3c9aa53ce0d03164108e894b3c79fe85d394b
eduardotecnologo <eduardotecnologo@hotmail.com>
2021-05-17 19:54:29 -0300

    Setup Project

---
+++ semana-javascript-expert04/app/pages/_shared/socketBuilder.js
@@ -0,0 +1,31 @@
+import { constants } from "./constants";
+
+constants
+export default class SocketBuilder{
+  constructor({ socketUrl, namespace }){
+    this.socketUrl = `${socketUrl}/${namespace}`;
+      this.OnUserConnected = () =>{}
+      this.OnUserDisconnected = () =>{}
+  }
+
+  setOnUserConnected(fn){
+    this.OnUserConnected = fn
+    return this;
+  }
+  setUserDisconnected(fn){
+    this.OnUserDisconnected = fn
+    return this;
+  }
+
+  build(){
+    const socket = globalThis.io.connect(this.socketUrl, {
+      withCredentials: false
+    });
+    socket.on('connection', () => console.log('Connected!'))
+
+    socket.on(constants.events.USER_CONNECTED, this.OnUserConnected)
+    socket.on(constants.events.USER_DISCONNECTED, this.OnUserDisconnected)
+
+    return socket;
+  }
+}
